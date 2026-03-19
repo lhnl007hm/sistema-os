2a25b9a7c612f6bd4f6d7587b6a66ae62edc0273
Luiz Henrique <social.luizh@gmail.com>
2026-03-19 16:19:22 -0300

    Analytics Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   const [session, setSession] = useState(null);
